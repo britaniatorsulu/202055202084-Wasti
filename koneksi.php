7ba98091f3f840787eb6bf0441a69863b9b35a66
britaniatorsulu <95205156+britaniatorsulu@users.noreply.github.com>
2022-01-27 04:49:21 -0800

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "202055202084_wasti_torsulu");
-
-if (mysqli_connect_errno()) {
-	echo "koneksi Database Gagal:".mysqli_connect_errno();
-}
-
-?>
